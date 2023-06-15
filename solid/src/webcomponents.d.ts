32bd84420c001211eb62395f773a73ecc31a53b2
Stijn van Meerten <s.meerten@avisi.nl>
2023-06-15 15:25:47 +0200

    Add explanation for webcomponents typings file
    Signed-off-by: Stijn van Meerten <s.meerten@avisi.nl>

--- solid/src/webcomponents.d.ts
+++ solid/src/webcomponents.d.ts
@@ -1,5 +1,7 @@
 import "solid-js"
 
+// We need to declare the WebComponents we use in the SolidJS namespace for Solid to understand them
+// https://github.com/solidjs/solid/issues/616
 declare module "solid-js" {
   namespace JSX {
     interface IntrinsicElements {
